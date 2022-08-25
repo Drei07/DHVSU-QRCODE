62aa88c764515af1d69305f9819d92def84ea042
ANDREI VISCAYNO <2018006164@dhvsu.edu.ph>
2022-08-25 08:52:10 +0300

    MAIN

--- dashboard/admin/authentication/admin-reset-password.php
+++ dashboard/admin/authentication/admin-reset-password.php
@@ -1,4 +1,5 @@
 <?php
+include_once '../../superadmin/controller/select-settings-coniguration-controller.php';
 require_once 'admin-class.php';
 $admin = new ADMIN();
 
@@ -51,6 +52,7 @@ if(isset($_GET['id']) && isset($_GET['code']))
 <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="shortcut icon" href="../../../src/img/<?php echo $logo ?>">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="../../../src/css/login.css?v=<?php echo time(); ?>">
     <title>Reset Password</title>
