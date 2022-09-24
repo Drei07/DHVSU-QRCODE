187f9bdaf774090f5b8c9bd52f95a5d26073cb6c
unknown <you@example.com>
2022-09-24 16:15:51 +0800

    LOCALHOST

--- database/dbconfig2.php
+++ database/dbconfig2.php
@@ -1,12 +1,6 @@
 <?php
 	try {
 
-		
-		// LOCALHOST
-		// $pdoConnect = new PDO("mysql:host=localhost;dbname=dhvsu", "root", "");
-
-
-		// LIVE
 		$pdoConnect = new PDO("mysql:host=localhost;dbname=u867039073_dhvsu_qrcode", "u867039073_dhvsu_qrcode", "Andreishania12");
 		$pdoConnect->setAttribute(PDO:: ATTR_ERRMODE, PDO:: ERRMODE_EXCEPTION);
 
